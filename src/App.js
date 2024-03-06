cc1d713d4e9a19b24d9c519518892b190c0318f5
Anton Knysh <anton@profairgames.com>
2024-03-06 12:04:00 +0200

    added functionality to the buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const handleMinusButton = () => {
+    count > 0 ? setCount(count - 1) : setCount(0);
+  };
+
+  const handlePlusButton = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>COUNTER:</h2>
+        <h1>{count}</h1>
+        <button onClick={handleMinusButton} className="minus">
+          - Минус
+        </button>
+        <button onClick={handlePlusButton} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
